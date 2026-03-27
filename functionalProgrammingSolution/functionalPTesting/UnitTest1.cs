31acc6ec051b9a815785e200b04db694918be600
Zitian Harper <Zitian.harper@icloud.com>
2026-03-27 10:34:23 +0000

    initial commit

---
+++ functionalProgrammingSolution/functionalPTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace functionalPTesting
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ functionalProgrammingSolution/functionalProgramming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace functionalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
